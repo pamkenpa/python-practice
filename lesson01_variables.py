44b77bca76633aceee1a8c5730f9a409ebbbca28
Jim <garchuchu0301@gmail.com>
2026-08-04 13:58:27 +0800

    Remove lesson01_variables.py

--- lesson01_variables.py
+++
@@ -1,4 +0,0 @@
-name = input("What's your name? ")
-age = int(input("How old are you? "))
-programming_language = input("What is your favorite programming language? ")
-print(f"Hey {name}, in 5 years you'll be {age + 5} and you like {programming_language}!")
