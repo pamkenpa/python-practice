4ed41cbcf9a0781db3adfa10b6614822c61130e5
Jim <gooreelaapants@gmail.com>
2026-08-12 01:34:53 +0800

    failed my exercise

--- library_checkout.py
+++ library_checkout.py
@@ -4,5 +4,43 @@ books = {
     "1984": {"copies": 5, "checked_out": 0}
 }
 
-def available_copies(book):
+def available_copies(books):
+    details = books[books]
+    return details["copies"] - details["checked_out"]
+        
   
+def is_available(books):
+    if available_copies(books) > 0:
+        return True
+    else:
+        return False
+
+
+def checkout_books(books, title):
+    try:
+        if title in books and is_available:
+            details = books[title]
+        return details["checked_out"] + 1 and True
+    except KeyError:
+        return False
+    
+    if title in books and is_available is False:
+        return False
+
+def library_report(books):
+    for book in books:
+        return {"The Hobbit:", available_copies(book), "available"}
+
+library_report(books)
+print(checkout_books(books, "Dune"))
+print(checkout_books(books, "The Hobbit"))
+library_report(books)
+print(checkout_books(books, "Nonexistent Book"))
+
+
+        
+        
+    
+       
+    
+
