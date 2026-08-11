1ab056b15abb0eb10205fa67669b170a16ca9302
Jim <gooreelaapants@gmail.com>
2026-08-11 20:29:02 +0800

    trying a new exercise

---
+++ library_checkout.py
@@ -0,0 +1,8 @@
+books = {
+    "The Hobbit": {"copies": 3, "checked_out": 1},
+    "Dune": {"copies": 2, "checked_out": 2},
+    "1984": {"copies": 5, "checked_out": 0}
+}
+
+def available_copies(book):
+  
