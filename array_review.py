6b2a3243fccabe2c8e2c6abed5dff62abbdeb9da
pamkenpa <158284027+pamkenpa@users.noreply.github.com>
2026-08-18 01:24:44 +0800

    Learn min/max, complete second_highest with two-tracker pattern

--- array_review.py
+++ array_review.py
@@ -19,3 +19,30 @@ print(temps)
 temps = [68, 72, 75, 70, 74]
 temps.remove(75)
 print(temps)
+
+prices = [45, 12, 78, 33, 9]
+print(max(prices))
+print(min(prices))
+
+path_a = 12
+path_b = 8
+
+print(max(path_a, path_b))
+print(min(path_a, path_b))
+
+temps = [68, 72, 75, 70, 74, 71, 73]
+
+def second_highest(arr):
+    highest = max(arr[0], arr[1])
+    second_highest = min(arr[0], arr[1])
+    for num in arr[2:]:
+        if num > highest:
+            second_highest = highest
+            highest = num
+        elif num > second_highest:
+            second_highest = num
+    return second_highest
+
+print(second_highest(temps))
+
+            
